0fd72f689377035a79ea31d2bc42dd3ad01b0bfa
branimir <branimir.mircetic@hotmail.com>
2022-01-27 11:15:10 +0100

    path

--- src/generate_dec.py
+++ src/generate_dec.py
@@ -3,12 +3,11 @@ import classla
 nlp = classla.Pipeline('sr')
 from deklinacije import deklinacije
 
-with open("../test/lista.txt",'r',encoding='utf8') as lista_reci:
-    with open("../ test/nova_lista.txt", 'w',encoding='utf8') as nova_lista_reci:
+with open("/home/branimir/Projects/algolia-sr/test/lista.txt",'r',encoding='utf8') as lista_reci:
+    with open("/home/branimir/Projects/algolia-sr/test/nova_lista.txt", 'w',encoding='utf8') as nova_lista_reci:
         for rec in lista_reci:
             print(rec)
             doc=nlp(rec.rstrip())
             doc_dict=doc.to_dict()[0][0][0]
             print(doc_dict)
             nova_lista_reci.write(" ".join(deklinacije(lemma=doc_dict['lemma'], pos=doc_dict['xpos']))+"\n")
-
